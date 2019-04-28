f49f3a4413064f069cfff7f7f8c2e0c7b515c5e9
Anthony Rurup <tcrurup@gmail.com>
2019-04-28 03:27:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,6 +7,10 @@
 
 
 def prime?(number)
-  number_range = (2..number-1).to_a
-  number_range.detect{ |test_number| number > 0 & number % test_number == 0} ? false : true
+  if number > 0 do
+    number_range = (2..number-1).to_a
+    number_range.detect{ |test_number| number % test_number == 0} ? false : true
+  else
+    false
+  end
 end
