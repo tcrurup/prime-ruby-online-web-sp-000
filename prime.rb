e33352d1b1802b1f5cb562f0fe0b32d85d7fc49a
Anthony Rurup <tcrurup@gmail.com>
2019-04-28 03:23:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,12 @@
+##
+#Grab all the numbers from 2 til the given number.  Since a prime can only be divided
+#by itself and one, I only want the numbers in between
+#Since we've eliminated the only numbers that it can be divided by, we can just test
+#and see if any numbers divide cleanly.  If so, then the number is NOT prime
+
+
+
 def prime(number)
-  number_range = (2..number).to_a
-  number_range.detect{ |number| }
+  number_range = (2..number-1).to_a
+  number_range.detect{ |test_number| number % test_number == 0} ? true : false
 end
